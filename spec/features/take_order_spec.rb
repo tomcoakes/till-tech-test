be96186715c3b7ea69278f7169b80923b2c3dcc3
Tom Coakes <tomcoakes@gmail.com>
2015-04-27 19:50:12 +0100

    Files created for models Order and Line

--- spec/features/take_order_spec.rb
+++ spec/features/take_order_spec.rb
@@ -2,12 +2,9 @@ feature "As a barista at the coffee shop, I can take an order" do
 
   describe "When a customer orders 1 Caffe Latte" do
     
-    before do
+    scenario "The line total is 4.75" do
       order = Order.new
       line = Line.new
-    end
-
-    scenario "The line total is 4.75" do
       order.add(:caffe_latte)
       expect(line.price_total).to eq "4.75"
     end
