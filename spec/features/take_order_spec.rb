72046d0c80b85dafc1da60a4563cf279313c9ea0
Tom Coakes <tomcoakes@gmail.com>
2015-04-28 09:29:25 +0100

    Removed order model, added printed_price method and hardcoded price list into line

--- spec/features/take_order_spec.rb
+++ spec/features/take_order_spec.rb
@@ -1,12 +1,22 @@
-feature "As a barista at the coffee shop, I can take an order" do
+feature "As a barista at the coffee shop, I can take an order." do
 
-  describe "When a customer orders 1 Caffe Latte" do
+  describe "When a customer orders 1 Caffe Latte," do
     
-    scenario "The line total is 4.75" do
-      order = Order.new
+    scenario "the line total is 4.75" do
       line = Line.new
-      order.add(:caffe_latte)
-      expect(line.price_total).to eq "4.75"
+      line.add(:caffe_latte)
+      expect(line.printed_price).to eq "4.75"
+    end
+
+  end
+
+  describe "When a customer orders 2 Caffe Lattes," do
+
+    scenario "the line total is 9.50" do
+      line = Line.new
+      line.add(:caffe_latte)
+      line.add(:caffe_latte)
+      expect(line.printed_price).to eq "9.50"
     end
 
   end
