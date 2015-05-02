9ac0fbfbf9bf1ebf3487502fcd68f562ee978e6a
Tom Coakes <tomcoakes@gmail.com>
2015-05-02 15:59:23 +0100

    Second feature test created that requires me to generate item model

--- spec/features/take_order_spec.rb
+++ spec/features/take_order_spec.rb
@@ -19,4 +19,23 @@ feature "As a barista at the coffee shop, I can take an order." do
 
   end
 
+  describe "When a customer orders 2 Caffe Lattes," do
+
+    let(:till) { Till.new }
+
+    before do
+      2.times { till.add_to_order(:caffe_latte) }
+    end
+
+    scenario "the current order contains 2 items" do
+      expect(till.current_order.count).to eq 2
+    end
+
+    scenario "the receipt total is 9.50" do
+      receipt = till.generate_receipt
+      expect(receipt.total_price).to eq 950
+    end
+
+  end
+
 end
