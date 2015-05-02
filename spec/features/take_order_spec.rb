fad9f17aaa0d4698edbc96f69611917e26c8e461
Tom Coakes <tomcoakes@gmail.com>
2015-05-02 15:04:59 +0100

    Deleted models and tests in order to start from scratch with a better approach

--- spec/features/take_order_spec.rb
+++ spec/features/take_order_spec.rb
@@ -1,30 +1,7 @@
 feature "As a barista at the coffee shop, I can take an order." do
 
   describe "When a customer orders 1 Caffe Latte," do    
-    scenario "the line total is 4.75" do
-      line = Line.new
-      line.add(:caffe_latte)
-      expect(line.printed_price).to eq "4.75"
-    end
-  end
-
-  describe "When a customer orders 2 Caffe Lattes," do
-    scenario "the line total is 9.50" do
-      line = Line.new
-      line.add(:caffe_latte)
-      line.add(:caffe_latte)
-      expect(line.printed_price).to eq "9.50"
-    end
-  end
-
-  describe "When a customer orders 2 Caffe Lattes and 1 Blueberry Muffin" do
-    scenario "the two product types appear on separate lines" do
-      receipt = Receipt.new
-      receipt.add(:caffe_latte)
-      receipt.add(:caffe_latte)
-      receipt.add(:blueberry_muffin)
-      expect(receipt.lines[0].printed_price).to eq "9.50"
-      expect(receipt.lines[1].printed_price).to eq "4.05"
+    scenario "the receipt total is 4.75" do
     end
   end
 
--- spec/unit/receipt_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "Receipt" do
-  
-  it "begins with no lines" do
-    receipt = Receipt.new
-    expect(receipt.lines).to be_empty
-  end
-
-end
