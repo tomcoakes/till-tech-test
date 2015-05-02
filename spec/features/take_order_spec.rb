36c05cf69257e2d217cd5825eb9efc9571efd1c6
Tom Coakes <tomcoakes@gmail.com>
2015-05-02 15:22:43 +0100

    First new feature test created for testing receipt generation from the till object

--- spec/features/take_order_spec.rb
+++ spec/features/take_order_spec.rb
@@ -2,6 +2,10 @@ feature "As a barista at the coffee shop, I can take an order." do
 
   describe "When a customer orders 1 Caffe Latte," do    
     scenario "the receipt total is 4.75" do
+      till = Till.new
+      till.add(:caffe_latte)
+      receipt = till.generate_receipt
+      receipt.total_price = 475
     end
   end
 
